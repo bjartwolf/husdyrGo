fe67b034892857fb91629f966e8be58f86b430f9
bjartwolf <bjartnes@gmail.com>
2013-08-09 09:26:24 +0200

    implementing some rules

--- husdyr.go
+++ husdyr.go
@@ -5,41 +5,54 @@ import "github.com/cznic/mathutil"
 import "sort"
 
 func indexOf (array sort.StringSlice, lookingFor string) int {
-	for i, value := range array {
-		if (value == lookingFor) {
-			return i
-		}
-	}
-	return -1
+  for i, value := range array {
+    if (value == lookingFor) {
+      return i
+    }
+  }
+  return -1
 }
 
 func main () {
-	persons := sort.StringSlice{"arne", "beb", "cecilie"}
-	drikke := sort.StringSlice{"kaffe", "te", "pils"}
-/*	for _, person := range persons {
-		fmt.Println(person)
-	}
+  persons := sort.StringSlice{"ukranian", "norwegian", "japanese", "spaniard", "englishman"}
+  drinks := sort.StringSlice{"milk", "tea", "water", "orangejuice", "coffee"}
+  colors := sort.StringSlice{"yellow", "blue", "red", "green", "ivory"}
+/*  for _, person := range persons {
+    fmt.Println(person)
+  }
 */
-	mathutil.PermutationFirst(persons)
-//	mathutil.PermutationFirst(drikke)
-	for personPerm, p := true, 0; personPerm ; personPerm, p = mathutil.PermutationNext(persons), p + 1 {
-		// Arne bor i det første huset
-//		if (persons[0] != "arne") {
-//			continue
-//		}
-		mathutil.PermutationFirst(drikke)
-		for drikk, drikkePerm := 0, true; drikkePerm; drikk, drikkePerm = drikk + 1,  mathutil.PermutationNext(drikke){
-			// Person i siste huset (n=2) drikker ikke te
-			if (drikke[2] == "te") {
-				continue
-			}
-			// Arne drikker te
-			if (indexOf(drikke, "te") != indexOf(persons, "arne")) {
-				continue
-			}
-			fmt.Println("******************")
-			fmt.Println(persons)
-			fmt.Println(drikke)
-		}
-	}
+  mathutil.PermutationFirst(persons)
+  mathutil.PermutationFirst(drinks)
+  mathutil.PermutationFirst(colors)
+  for personPerm := true; personPerm ; personPerm = mathutil.PermutationNext(persons) {
+    // Rule #1: Norweigan lives in first house 
+    if (persons[0] != "norwegian") {
+      continue
+    }
+    for colorsPerm:= true; colorsPerm; colorsPerm= mathutil.PermutationNext(colors){
+      // #Rule #2: Englishman lives in red house
+      if (indexOf(colors, "red") != indexOf(persons, "englishman")) {
+        continue
+      }
+      // #Rule #6: Green house is to the right of ivory house
+      if (indexOf(colors, "green") - indexOf(colors, "ivory") != 1) {
+        continue
+      }
+
+      for drinksPerm := true; drinksPerm; drinksPerm =  mathutil.PermutationNext(drinks){
+      // Person i siste huset (n=2) drinksr ikke te
+//      if (drinks[2] == "te") {
+//        continue
+//      }
+      // Arne drinksr te
+//      if (indexOf(drinks, "te") != indexOf(persons, "arne")) {
+//        continue
+//      }
+      fmt.Println("******************")
+      fmt.Println(persons)
+      fmt.Println(drinks)
+      fmt.Println(colors)
+      }
+    }
+  }
 }
