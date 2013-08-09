b29c48665b576f52764bbd431762ac459b6c5a64
bjartwolf <bjartnes@gmail.com>
2013-08-09 08:18:00 +0200

    init

---
+++ husdyr.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "github.com/cznic/mathutil"
+import "sort"
+
+func main () {
+	persons := sort.StringSlice{"arne", "beb", "cecilie"}
+/*	drikke := []string{"kaffe", "te", "pils"}
+	for _, person := range persons {
+		fmt.Println(person)
+	}
+*/
+	mathutil.PermutationFirst(persons)
+	morePermutations := true 
+	for (morePermutations) {
+		fmt.Println(persons)
+		morePermutations = mathutil.PermutationNext(persons)
+	}
+}
