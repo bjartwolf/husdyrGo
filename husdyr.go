53164cacd4636472e56316a3186833369590aab3
bjartwolf <bjartnes@gmail.com>
2013-08-09 13:13:41 +0200

    added indexofq

--- husdyr.go
+++ husdyr.go
@@ -15,31 +15,30 @@ func indexOf (array sort.StringSlice, lookingFor string) int {
 
 func main () {
   persons := sort.StringSlice{"ukranian", "norwegian", "japanese", "spaniard", "englishman"}
-  drinks := sort.StringSlice{"milk", "tea", "water", "orangejuice", "coffee"}
-  colors := sort.StringSlice{"yellow", "blue", "red", "green", "ivory"}
-/*  for _, person := range persons {
-    fmt.Println(person)
-  }
-*/
   mathutil.PermutationFirst(persons)
-  mathutil.PermutationFirst(drinks)
-  mathutil.PermutationFirst(colors)
   for personPerm := true; personPerm ; personPerm = mathutil.PermutationNext(persons) {
     // Rule #1: Norweigan lives in first house 
     if (persons[0] != "norwegian") {
       continue
     }
+    colors := sort.StringSlice{"yellow", "blue", "red", "green", "ivory"}
+    mathutil.PermutationFirst(colors)
     for colorsPerm:= true; colorsPerm; colorsPerm= mathutil.PermutationNext(colors){
-      // #Rule #2: Englishman lives in red house
+      // Rule #2: Englishman lives in red house
       if (indexOf(colors, "red") != indexOf(persons, "englishman")) {
         continue
       }
-      // #Rule #6: Green house is to the right of ivory house
+      // Rule #6: Green house is to the right of ivory house
       if (indexOf(colors, "green") - indexOf(colors, "ivory") != 1) {
         continue
       }
-
+      drinks := sort.StringSlice{"milk", "tea", "water", "orangejuice", "coffee"}
+      mathutil.PermutationFirst(drinks)
       for drinksPerm := true; drinksPerm; drinksPerm =  mathutil.PermutationNext(drinks){
+        // Rule #Ukranian drinks tea 
+        if (indexOf(drinks, "tea") != indexOf(persons, "ukranian")) {
+          continue
+        }
       // Person i siste huset (n=2) drinksr ikke te
 //      if (drinks[2] == "te") {
 //        continue
@@ -48,10 +47,10 @@ func main () {
 //      if (indexOf(drinks, "te") != indexOf(persons, "arne")) {
 //        continue
 //      }
-      fmt.Println("******************")
-      fmt.Println(persons)
-      fmt.Println(drinks)
-      fmt.Println(colors)
+        fmt.Println("******************")
+        fmt.Println(persons)
+        fmt.Println(drinks)
+        fmt.Println(colors)
       }
     }
   }
